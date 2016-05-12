93f10d969a2862a5428fc4066703481e3775f9e7
Vanessa Lin <vyslin@users.noreply.github.com>
2016-05-11 22:32:46 -0700

    reads each line from file

--- src/main/LanguageCensor.java
+++ src/main/LanguageCensor.java
@@ -1,23 +1,56 @@
 package main;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.io.*;
+
 
 public class LanguageCensor {
-	public void main(String args[]) {
+	public LanguageCensor(){
 		
-		Set<String> data = readDataFile();
+	}
+	public static void main(String args[]) throws IOException {
 		
+		LanguageCensor censorText = new LanguageCensor();
+		Set<String> swearWords = censorText.readDataFile();
 		
-		
-
 	}
 
-
-	// Input: nothing
-	// Ouput: a set of all the words in String
-	// This method read from the data.txt and return a set of all the words.
-	private Set<String> readDataFile() {
+	/**
+	 * This method generates a list of swear words.
+	 * @return a set of swear words
+	 */
+	private Set<String> readDataFile() throws IOException {
 		// TODO
+		Set<String> swearWordList = new HashSet<String>(); // TODO: or initialize swearWordList as null and create a set object the first time we put a word in
+		FileReader in = null;
+		
+		try {
+			in = new FileReader("swear_word_list.txt");
+			BufferedReader br = new BufferedReader(in);
+		    String line;
+		    
+		    
+		    while ((line = br.readLine()) != null) {
+		       //System.out.println(line);
+		       
+		       char firstChar = line.charAt(0);
+		       
+		       // We ignore all the terms with multiple words, which are
+		       // marked by quotes around the term
+		       if(firstChar != '"'){
+		    	   System.out.println(line);
+		       }
+		    }
+		    br.close();
+		}
+		finally {
+	         if (in != null) {
+	            in.close();
+	         }
+	      }
+		
+		
 		return null;
 	}
 }
